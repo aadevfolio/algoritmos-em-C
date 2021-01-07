f501d1c2612613b727f3c8924d2abac23d18d2f7
Aline <alineprofissional@outlook.com>
2021-01-07 15:42:51 -0300

    update da passagem por valor

--- nivel 3/passagem_valor.c
+++ nivel 3/passagem_valor.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-void convesor(float n);
+void incrementa (int n);
 int main(){
     // passagem por valor - o valor parametro só subtituido no subprograma, assim que retorna para o programa principal, o valor permanece inalterado
 
-    float cm;
-    printf("Centimetros: ");
-    scanf("%f", &cm);
-    convesor(cm); // Passagem por valor, sofre alterações dentro do subprograma, mas quando retorna para o programa, o valor não é alterado
-    printf("Cm: %.2f", cm); 
+    int num = 10;
+    incrementa(num); // Passagem por valor, sofre alterações dentro do subprograma
+    printf("Valor na Main: %d", num); // quando retorna para o programa, o valor não é alterado
 
     return 0; 
 }
 
-void convesor(float n){
-    float resultado;
-    resultado = n * 10;
-    printf("Milimetros: %.2f\n", resultado);
+void incrementa (int n){
+    n = n + 1;
+    printf("Valor no Subprograma: %d\n", n);
 }
